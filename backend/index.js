231660ba837573f61b33414215e2f942b8cf862c
Dimitris Nimas <hello@dimitrisnimas.gr>
2026-01-10 02:14:58 +0200

    Implement scheduler

--- backend/index.js
+++ backend/index.js
@@ -61,9 +61,14 @@ fastify.ready().then(() => {
     });
 });
 
+import { startScheduler } from './utils/scheduler.js';
+
 // Start Server
 const start = async () => {
     try {
+        // Start Scheduler
+        startScheduler();
+
         const PORT = process.env.PORT || 3000;
         await fastify.listen({ port: PORT, host: '0.0.0.0' });
     } catch (err) {
